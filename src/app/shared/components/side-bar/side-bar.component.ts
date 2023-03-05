305751ade1dcd4bb605a24dd95e74e07cdd920dc
SalmaDarwish <salmadarwish797@gmail.com>
2023-03-05 23:24:50 +0200

    dropdown

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-bar.component.scss']
 })
 export class SideBarComponent {
-
+icon=document.getElementById("toggle-icon");
+toggleIcon(){
+  
+  document.getElementById("toggle-icon")?.classList.toggle("fa-angle-up")
+}
 }
